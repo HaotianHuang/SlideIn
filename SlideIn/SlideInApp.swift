333d6cb4933e8a3c69c2e2b9c2df6377410f1449
Haotian Huang <haotianhuang@gmail.com>
2021-06-15 17:21:27 +1000

    Initial Commit

---
+++ SlideIn/SlideInApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideInApp.swift
+//  SlideIn
+//
+//  Created by Haotian Huang on 15/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SlideInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
