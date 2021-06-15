3aca0428d127367b5f2f84593838aa86828b5e35
Haotian Huang <haotianhuang@gmail.com>
2021-06-15 17:46:51 +1000

    -add sample array and button to randomise text

--- SlideIn/ContentView.swift
+++ SlideIn/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State var array = ["Don't go through the front door", "Make sure you are nice", "Heck Yes"]
+
+    @State var num = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 5) {
+            Text(array[num])
+            Button(action: {
+                let newNum = Int.random(in: 0..<array.count)
+                if newNum != num {
+                    num = newNum
+                }
+                if newNum == num {
+                    num = Int.random(in: 0..<array.count)
+                }
+            }, label: {
+                Text("Get Inspo 🥺")
+            })
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 11")
     }
 }
+
